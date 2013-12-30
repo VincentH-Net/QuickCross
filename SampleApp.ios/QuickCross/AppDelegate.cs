5c707c2a4c524262a9a8b0fca2cbdcb95df121eb
Vincent Hoogendoorn <vincent.hoogendoorn@macaw.nl>
2013-12-30 14:44:36 +0100

    iOS completed build settings in VS

--- SampleApp.ios/QuickCross/AppDelegate.cs
+++ SampleApp.ios/QuickCross/AppDelegate.cs
@@ -1,4 +1,3 @@
-#if TEMPLATE // To add a navigator class: in the Visual Studio Package Manager Console (menu View | Other Windows), enter "Install-Mvvm". Alternatively: copy this file, then in the copy remove the enclosing #if TEMPLATE ... #endif lines and replace _APPNAME_ with the application name.
 using MonoTouch.UIKit;
 using SampleApp.Shared;
 
@@ -19,4 +18,3 @@ namespace SampleApp.ios
 	// TODO: Add the following code to Application_Activated:
 	//    Ensure_APPNAME_Application(RootFrame);
 }
-#endif // TEMPLATE
