61391f924132f0ddb703a6cef14ee121ed7d5a18
Vincent Hoogendoorn <vincent.hoogendoorn@macaw.nl>
2014-01-15 12:40:24 +0100

    GitHub ReadMe - QuickCross pattern updated

--- SampleApp.android/SampleAppNavigator.cs
+++ SampleApp.android/SampleAppNavigator.cs
@@ -33,5 +33,13 @@ namespace SampleApp
         {
             Navigate(typeof(SampleItemView));
         }
+
+        /* TODO: For each view, add a method to navigate to that view like this:
+
+        public void NavigateTo_VIEWNAME_View()
+        {
+            Navigate(typeof(_VIEWNAME_View));
+        }
+         * Note that the New-View command adds the above code automatically (see http://github.com/MacawNL/QuickCross#new-view). */
     }
 }
